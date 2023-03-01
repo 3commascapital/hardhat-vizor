784105282e8f66854c3060e42af31b8ef25004aa
3commascapital <3commascapital@github.com>
2023-03-01 13:43:45 -0600

    Updates error parsing to return single value

--- src/error.ts
+++ src/error.ts
@@ -1,55 +1,89 @@
 import * as ethers from 'ethers'
-import { EthersError, getParsedEthersError } from "@enzoferey/ethers-error-parser";
+import { EthersError, ReturnValue, getParsedEthersError } from "@enzoferey/ethers-error-parser";
 import _ from 'lodash';
 
-export const fragmentFromId = (intrfce: ethers.utils.Interface, _id: string) => {
-  const id = _id.slice(0, 10)
-  return Object.entries(intrfce.errors).find(([signature, fragment]) => {
-    return ethers.utils.id(signature).slice(0, 10) === id
-  })
+type FragmentBundle = {
+  interface?: ethers.utils.Interface;
+  signature: string;
+  fragment?: ethers.utils.ErrorFragment;
 }
 
-export const fromError = (err: EthersError, intrfce?: ethers.utils.Interface) => {
-  const parsed = getParsedEthersError(err)
-  if (!intrfce || parsed.errorCode !== "UNKNOWN_ERROR") {
-    return parsed
+export const fragmentFromId = (interfaces: ethers.utils.Interface[], _id: string): FragmentBundle => {
+  const id = _id.slice(0, 10)
+  for (const intrfce of interfaces) {
+    const found = Object.entries(intrfce.errors).find(([signature, fragment]) => {
+      return ethers.utils.id(signature).slice(0, 10) === id
+    })
+    if (found) {
+      return {
+        interface: intrfce,
+        signature: found[0],
+        fragment: found[1],
+      }
+    }
   }
-  const anyError = err as any
-  const data = anyError?.data
-    || anyError?.error?.data
-    || anyError?.error?.error?.data
-    || anyError?.error?.error?.error?.data
-  return new CustomInterfaceError(data, intrfce)
+  return {
+    signature: '',
+  }
+}
+
+export const fromError = (err: EthersError, intrfce: ethers.utils.Interface | ethers.utils.Interface[] = []) => {
+  return new CustomInterfaceError(err, _.isArray(intrfce) ? intrfce : [intrfce])
 }
 
 export type ErrorArgValue = string | ethers.BigNumber | boolean
 export type ErrorArgKey = string | number
 export type ErrorArgMap = Map<string | number, ErrorArgValue>
 
+const defaultKeyValue = () => new Map<ErrorArgKey, ErrorArgValue>()
+
+const defaultSetup = (cError: CustomInterfaceError, parsed: ReturnValue) => {
+  cError.signature = ''
+  cError.fragment = null
+  cError.args = defaultKeyValue()
+  cError.errorCode = parsed.errorCode
+  cError.context = parsed.context || ''
+}
+
 export class CustomInterfaceError {
-  args: ErrorArgMap;
+  intrfce!: ethers.utils.Interface | null;
+  args!: ErrorArgMap;
   errorCode = 'CUSTOM_ERROR'
-  signature: string
-  fragment: ethers.utils.ErrorFragment
+  context = 'custom error'
+  signature!: string
+  fragment!: ethers.utils.ErrorFragment | null
   constructor(
-    protected data: string,
-    protected intrfce: ethers.utils.Interface,
+    protected err: EthersError,
+    interfaces: ethers.utils.Interface[],
   ) {
-    const found = fragmentFromId(intrfce, data)
-    if (!found) {
-      throw new Error('unable to find fragment')
+    const anyError = err as any
+    const defaultKeyValueArgs = new Map<ErrorArgKey, ErrorArgValue>()
+    const data = anyError?.data
+      || anyError?.error?.data
+      || anyError?.error?.error?.data
+      || anyError?.error?.error?.error?.data
+      || anyError?.error?.error?.error?.error?.data
+    const parsed = getParsedEthersError(err)
+    if (!interfaces.length || parsed.errorCode !== "UNKNOWN_ERROR") {
+      defaultSetup(this, parsed)
+      return
     }
-    const [signature, fragment] = found
+    const { interface: intrfce, signature, fragment } = fragmentFromId(interfaces, data)
+    if (!signature) {
+      defaultSetup(this, parsed)
+      return
+    }
+    this.intrfce = intrfce || null
     const argsData = data.slice(10)
-    const keyValueArgs = fragment.inputs.reduce((args, paramType, index) => {
+    const keyValueArgs = fragment?.inputs.reduce((args, paramType, index) => {
       const contentLength = 64
       const contentStart = index * contentLength
       const content = argsData.slice(contentStart, contentStart + contentLength)
       const argAsValue = parseArg(paramType.type, content)
       args.set(paramType.name, argAsValue)
       return args
-    }, new Map<ErrorArgKey, ErrorArgValue>())
-    this.fragment = fragment
+    }, defaultKeyValueArgs) || defaultKeyValueArgs
+    this.fragment = fragment || null
     this.signature = signature
     this.args = new Map(_.flatMap([...keyValueArgs.entries()], ([key, value], index) => ([
       [key, value],
@@ -61,7 +95,7 @@ export class CustomInterfaceError {
     return this.intrfce
   }
   name() {
-    return this.fragment.name
+    return this.fragment?.name || null
   }
   argSeries() {
     return [...this.args.entries()].reduce((series, [key, value]) => {
@@ -101,7 +135,7 @@ export const parseArg = (type: string, content: string) => {
     return ethers.BigNumber.from(`0x${content}`)
   }
   if (type.includes('int')) {
-    return ethers.BigNumber.from(`0x${content}`).sub(ethers.constants.MaxInt256.div(2))
+    return ethers.BigNumber.from(`0x${content}`).sub(ethers.constants.MaxInt256)
   }
   if (type === 'address') {
     return ethers.utils.getAddress(`0x${content.slice(64-40)}`)
