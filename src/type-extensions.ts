55835868e6d93ab4aba647779f76212a488d0a1c
3commascapital <3commascapital@github.com>
2023-08-17 18:30:15 -0500

    Updates imports

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -3,6 +3,7 @@
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import "hardhat/types/config";
 import "hardhat/types/runtime";
+import '@nomiclabs/hardhat-ethers'
 
 import { Vizor } from "./Vizor";
 
